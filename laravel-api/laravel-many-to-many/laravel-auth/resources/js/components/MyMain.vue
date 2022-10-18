ba2ea4100fd5943aada459beef43650dfe4fded1
Stefano Morgia <ste.morgia@gmail.com>
2022-10-18 15:57:35 +0200

    progetto clonato + dipendenze installate

---
+++ laravel-api/laravel-many-to-many/laravel-auth/resources/js/components/MyMain.vue
@@ -0,0 +1,69 @@
+<template>
+    <div class="container">
+        <h1 class="mb-5">Posts list</h1>
+
+        <div v-if="loading" class="d-flex justify-content-center">
+            <div class="spinner-border" role="status">
+                <span class="sr-only">Loading...</span>
+            </div>
+        </div>
+        <div v-else class="row">
+            <div class="card col-12 mb-5" v-for="(post, index) in posts" :key="index">
+                <img :src="post.cover" class="card-img-top">
+                <div class="card-body">
+                    <h5 class="card-title">{{post.title}}</h5>
+                    <p class="card-text">{{post.content}}</p>
+                    <p class="card-text">{{post.category?post.category.name:'-'}}</p>
+                    <a href="#" class="btn btn-primary">Read more...</a>
+                </div>
+            </div>
+        </div>
+
+
+        <nav>
+            <ul class="pagination">
+                <li class="page-item" :class="(currentPage==1?'disabled':'')" ><a class="page-link" href="#" @click.prevent="getPosts(currentPage - 1)">Previous</a></li>
+                <li class="page-item disabled"><span class="page-link" href="#">{{currentPage}}/{{lastPage}}</span></li>
+                <li class="page-item" :class="(currentPage==lastPage)?'disabled':''"><a class="page-link" href="#" @click.prevent="getPosts(currentPage + 1)">Next</a></li>
+            </ul>
+
+        </nav>
+
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'MyMain',
+        data() {
+            return {
+                posts: [],
+                currentPage: 1,
+                lastPage: null,
+                loading: true
+            }
+        },
+        methods: {
+            getPosts(page) {
+                this.loading = true;
+                axios.get('/api/posts', {
+                    params: {
+                        page: page
+                    }
+                })
+                .then((response) => {
+                    this.posts = response.data.results.data;
+                    this.loading = false;
+                    this.currentPage = response.data.results.current_page;
+                    this.lastPage = response.data.results.last_page;
+                });
+            },
+        },
+        mounted() {
+            this.getPosts(1);
+        }
+    }
+</script>
+
+<style>
+</style>
---
+++ laravel-api/laravel-many-to-many/laravel-auth/resources/views/admin/posts/edit.blade.php
@@ -0,0 +1,47 @@
+@extends('layouts.app')
+
+@section('content')
+
+    <div class="container">
+
+        <form action="{{route('admin.posts.update', ['post' => $post->id])}}" method="POST">
+
+            @csrf
+            @method('PUT')
+
+            <h1 class="mb-4">Edit post</h1>
+
+            <select name="category_id" class="form-control @error('category_id') is-invalid @enderror" id="category_id">
+                <option {{(old('category_id')=="")?'selected':''}} value="">Nessuna categoria</option>
+                @foreach ($categories as $category)
+                    <option {{(old('category_id', $post->category_id)==$category->id)?'selected':''}} value="{{ $category->id }}">{{ $category->name }}</option>
+                @endforeach
+            </select>
+
+            <div class="form-group mb-3">
+                <label for="title">Title</label>
+                <input type="text" class="form-control @error('title') is-invalid @enderror" required id="title" name="title" max="255" value="{{old('title', $post->title)}}">
+                @error('title')
+                    <div class="invalid-feedback">
+                        {{$message}}
+                    </div>
+                @enderror
+            </div>
+
+            <div class="form-group mb-3">
+                <label for="content">Content</label>
+                <textarea class="form-control  @error('content') is-invalid @enderror" required id="content" name="content">{{old('content', $post->content)}}</textarea>
+                @error('content')
+                    <div class="invalid-feedback">
+                        {{$message}}
+                    </div>
+                @enderror
+            </div>
+
+            <button type="submit" class="mt-3 btn btn-primary">Update</button>
+
+        </form>
+
+    </div>
+
+@endsection
